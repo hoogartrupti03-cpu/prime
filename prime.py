301055cdd1c735b4eab516d393e5b3ba0b48eee9
hoogartrupti03-cpu <hoogartrupti03@gmail.com>
2026-01-19 12:08:29 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,6 +1,9 @@
-def is_prime_numbers():
-    return n > 1 and all(n % i for i in range(2, int(n**0.5) + 1))
 
-if __name__ == "__main__":
-    print("Prime number:")
-    
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
