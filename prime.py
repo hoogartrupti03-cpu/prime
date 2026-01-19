eb49f750f90cba6e47b196d151e487cb18849165
hoogartrupti03-cpu <hoogartrupti03@gmail.com>
2026-01-19 12:34:01 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,6 +1,7 @@
 def is_prime_numbers():
     return n > 1 and all(n % i for i in range(2, int(n**0.5) + 1))
-
+    
+if __name__ == "__main__":
     print(f"{n} is a prime number.")
 else:
     print(f"{n} is not a prime number.")
