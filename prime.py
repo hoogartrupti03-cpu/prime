69c903310fa2e067556ea22fd187ee1f302124a4
truptihoogar03-cpu <hoogartrupti03@gmail.com>
2026-01-19 11:05:57 +0530

    save

---
+++ prime.py
@@ -0,0 +1,6 @@
+def is_prime_numbers():
+    return if (n>2):
+       for i in range(n,2)
+        if n % 1==0 :
+        if __name__ "=="__main__():
+print("Prime number:")
---
+++ test_prime.py
@@ -0,0 +1,8 @@
+from pime_number import is_prime
+def test_is_prime 1()
+assert_is_prime (5)=="prime"
+def test_is_prime 2()
+assert_is_prime (7)=="prime"
+def test_is_prime 3()
+assert_is_prime (10)==" not prime"
+
