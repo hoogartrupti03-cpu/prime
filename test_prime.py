6ae6793d004c2fb01da55f29e905c32c5515df4f
hoogartrupti03-cpu <hoogartrupti03@gmail.com>
2026-01-19 12:50:09 +0530

    Update test_prime.py

--- test_prime.py
+++ test_prime.py
@@ -1,9 +1,9 @@
 from prime_number import is_prime
-def test_is_prime 1()
+def test_is_prime()
   assert_is_prime (5)=="prime"
-def test_is_prime 2()
+def test_is_prime()
   assert_is_prime (7)=="prime"
-def test_is_prime 3()
+def test_is_prime()
   assert_is_prime (10)==" Not prime"
 
 
